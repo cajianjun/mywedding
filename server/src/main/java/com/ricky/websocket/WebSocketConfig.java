3f872567ad89f588b6683a4962a0f53c30e116ec
DESKTOP-4P6MLVA\10308 <1030833228@qq.com>
2018-03-23 16:43:55 +0800

    commit

--- server/src/main/java/com/ricky/websocket/WebSocketConfig.java
+++ server/src/main/java/com/ricky/websocket/WebSocketConfig.java
@@ -1,26 +1,39 @@
-package com.cjj.chat;
+package com.ricky.websocket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
+/**
+ * socket核心配置容器
+ */
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        //namespace 返回数据时加/chat
-        config.enableSimpleBroker("/chat");
-        //namespace 经过controller的方法的路径都要加/app
+        config.enableSimpleBroker("/topic");// /users 默认通知
         config.setApplicationDestinationPrefixes("/app");
+        //设置前缀  默认是user 可以修改  点对点时使用
+        config.setUserDestinationPrefix("/ricky/");
     }
 
-    //注册websocket
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").withSockJS();
+            registry.addEndpoint("/ricky-websocket").withSockJS();
     }
 
+
+    @Bean
+    public SocketSessionRegistry SocketSessionRegistry(){
+        return new SocketSessionRegistry();
+    }
+    @Bean
+    public STOMPConnectEventListener STOMPConnectEventListener(){
+        return new STOMPConnectEventListener();
+    }
 }
